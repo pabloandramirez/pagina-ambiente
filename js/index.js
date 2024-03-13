7782468b49fa97dd6851ece8c2039b057f9bdef1
Pablo <pablo.and.ramirez@outlook.com>
2024-03-13 17:21:26 -0300

    se rediseña espacios para un sitio web mas compacto, tambien el orden de colores

--- js/index.js
+++ js/index.js
@@ -62,7 +62,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         let offset = 0 // Desplazamiento de 75px hacia arriba
         
         if(window.innerWidth <= 768){
-          offset += 75;
+          offset += 62;
         }
         window.scrollTo({
           top: targetElement.offsetTop - offset, // Posición de desplazamiento
