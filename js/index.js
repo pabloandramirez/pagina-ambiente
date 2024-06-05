becaf9a61eeed591665eb842aab9f4e2a37a3a0e
Pablo <pablo.and.ramirez@outlook.com>
2024-06-05 11:22:08 -0300

    se crea la busqueda de normativas por direccion, mediante consultas a una api-rest

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+const urlNormativas = 'http://127.0.0.1:8080/normativa/';
+
 document.addEventListener("DOMContentLoaded", function(){
   cargarNoticias();
 } );
@@ -66,19 +68,19 @@ async function cargarNoticias() {
 }
 
 
-var selectElement = document.getElementById('select-opciones');
-var hiddenInputElement = document.getElementById('input-categoria');
+// var selectElement = document.getElementById('select-opciones');
+// var hiddenInputElement = document.getElementById('input-categoria');
 
-// Establecer el valor predeterminado del campo oculto al valor seleccionado por defecto
-hiddenInputElement.value = selectElement.value;
+// // Establecer el valor predeterminado del campo oculto al valor seleccionado por defecto
+// hiddenInputElement.value = selectElement.value;
 
-// Escuchar el evento de cambio en el select
-selectElement.addEventListener('change', function() {
-  hiddenInputElement.value = this.value;
-});
+// // Escuchar el evento de cambio en el select
+// selectElement.addEventListener('change', function() {
+//   hiddenInputElement.value = this.value;
+// });
 
-var selectElement2 = document.getElementById('select-opciones-2');
-var hiddenInputElement2 = document.getElementById('input-categoria-2');
+// var selectElement2 = document.getElementById('select-opciones-2');
+// var hiddenInputElement2 = document.getElementById('input-categoria-2');
 
 // Establecer el valor predeterminado del campo oculto al valor seleccionado por defecto
 // hiddenInputElement2.value = selectElement2.value;
@@ -292,3 +294,45 @@ $(document).ready(function () {
   $(".form-control").each(countCharacters);
   $(".form-control").keyup(countCharacters);
 });
+
+document.getElementById('btn-buscar-normativas').addEventListener('click', async function(e) {
+  try{
+    e.preventDefault();
+    const stringDireccion = document.getElementById('select-opciones').value;
+    const containerResultados = document.getElementById('resultados-normativas');
+
+
+    await fetch(urlNormativas+"?direccion="+`${stringDireccion}`)
+    .then(response => response.json())
+    .then(data => {
+      const resultadosNormativas = data;
+      containerResultados.innerHTML='';
+
+      resultadosNormativas.forEach((result, index) =>{
+        var{titulo, direccion, urlDocumento} = result;
+
+        const anchord = document.createElement('a');
+        anchord.classList.add('enlace-normativa');
+        anchord.setAttribute('href', urlDocumento);
+        anchord.setAttribute('target', '_blank')
+        anchord.innerHTML=`
+            <svg xmlns="http://www.w3.org/2000/svg" width="5rem" height="5rem" fill="currentColor" class="bi bi-filetype-pdf" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M14 4.5V14a2 2 0 0 1-2 2h-1v-1h1a1 1 0 0 0 1-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5zM1.6 11.85H0v3.999h.791v-1.342h.803q.43 0 .732-.173.305-.175.463-.474a1.4 1.4 0 0 0 .161-.677q0-.375-.158-.677a1.2 1.2 0 0 0-.46-.477q-.3-.18-.732-.179m.545 1.333a.8.8 0 0 1-.085.38.57.57 0 0 1-.238.241.8.8 0 0 1-.375.082H.788V12.48h.66q.327 0 .512.181.185.183.185.522m1.217-1.333v3.999h1.46q.602 0 .998-.237a1.45 1.45 0 0 0 .595-.689q.196-.45.196-1.084 0-.63-.196-1.075a1.43 1.43 0 0 0-.589-.68q-.396-.234-1.005-.234zm.791.645h.563q.371 0 .609.152a.9.9 0 0 1 .354.454q.118.302.118.753a2.3 2.3 0 0 1-.068.592 1.1 1.1 0 0 1-.196.422.8.8 0 0 1-.334.252 1.3 1.3 0 0 1-.483.082h-.563zm3.743 1.763v1.591h-.79V11.85h2.548v.653H7.896v1.117h1.606v.638z"/>
+            </svg>
+            <h3>${titulo}</h3>
+        `;
+        containerResultados.appendChild(anchord);
+      })
+    }
+    )
+    .catch(error => {
+      console.error('Hubo un error al obtener los datos:', error);
+    });
+
+    return Promise.resolve();
+  } catch(error){
+    return Promise.reject(error); // Rechazar la promesa en caso de error
+  }
+
+
+})
